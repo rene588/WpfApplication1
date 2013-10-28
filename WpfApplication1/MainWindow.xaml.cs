86085883e725b05bd0c436d9fc4353e83d50f5f3
rene588 <e.aquino.taipe@isur.edu.pe>
2013-10-28 17:47:28 -0500

    mm

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*
+            double screen = SystemParameters.FullPrimaryScreenHeight;
+            double screenw = SystemParameters.FullPrimaryScreenWidth;
+            this.Top = (screen - this.Height) / 2;
+            this .Top =(screenw - this .Width )/2;
+            
+
+            double workh = SystemParameters.WorkArea.Height;
+            double workw = SystemParameters.WorkArea.Width;
+            this.Top =(workh -this.Height )/2;
+            this.Left   =(workw - this.Width)/2;
+             */
+            /*--nuesta solo la barrita*/
+            try
+            {
+                Rect bounds = Properties.Settings.Default.posicion;
+                this.Top = bounds.Top;
+                this.Left = bounds.Left;
+
+                if (this.SizeToContent == SizeToContent.Manual)
+                {
+                    this.Width = bounds.Width;
+                    this.Height = bounds.Height;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("nnnnnnnnnnn.");
+            }
+             
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.posicion = this .RestoreBounds ;
+            Properties.Settings.Default.Save();
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            ventana2 v2 = new ventana2();
+            v2.Owner  = this;
+            v2.Show ();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            dialogo dialog = new dialogo();
+            dialog.Owner  = this;
+            //dialog.showdialog();
+            if (dialog.ShowDialog()== true )
+            {
+
+           
+            }
+
+
+
+      
+    }
+}
---
+++ WpfApplication1/dialogo.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Lógica de interacción para dialogo.xaml
+    /// </summary>
+    public partial class dialogo : Window
+    {
+        public String respuesta = "";
+        public dialogo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this .respuesta = "hola";
+            //DialogResult = true; 
+            this.Close();
+
+        }
+
+
+        internal bool showdialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
